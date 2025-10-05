d7a2d2d37bc7d4dd46388858922c792e3fdb25e4
hahaha <hahaha0417@hotmail.com>
2025-10-05 18:11:24 +0800

    fix

--- c_sharp_base/Program.cs
+++ c_sharp_base/Program.cs
@@ -11,7 +11,31 @@ namespace c_sharp_base
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new hahaha_form_main());
+
+            // ------------------------------------------
+            // 初始化環境
+            // ------------------------------------------
+            hahaha.Initial_Environment();
+            hahaha_form_main form_main_ = new hahaha_form_main();
+            ha.Form_Main = form_main_;
+
+            // ------------------------------------------
+            // 初始化
+            // ------------------------------------------
+            hahaha.Initial();
+            // ------------------------------------------
+            // 初始化 UI
+            // ------------------------------------------
+            hahaha.Initial_UI();
+
+            // ------------------------------------------
+            // 
+            // ------------------------------------------
+            // ------------------------------------------
+            // 
+            // ------------------------------------------
+
+            Application.Run(form_main_);
         }
     }
 }
---
+++ c_sharp_base/form/hahaha_form_main.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace c_sharp_base
+{
+    partial class hahaha_form_main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel_title.SuspendLayout();
+            panel_base.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel_title
+            // 
+            panel_title.Size = new Size(812, 65);
+            // 
+            // panel_base
+            // 
+            panel_base.Size = new Size(812, 508);
+            // 
+            // panel_body
+            // 
+            panel_body.Size = new Size(812, 443);
+            // 
+            // panel_title_system
+            // 
+            panel_title_system.Location = new Point(594, 0);
+            // 
+            // hahaha_form_main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(832, 528);
+            Name = "hahaha_form_main";
+            Text = "Form1";
+            FormClosing += hahaha_form_main_FormClosing;
+            Load += hahaha_form_main_Load;
+            panel_title.ResumeLayout(false);
+            panel_base.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
